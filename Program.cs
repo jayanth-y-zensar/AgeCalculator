4e5ca716910e07faeb2d5193a653907651cb1f0b
jayanth_y <jayanth.yedureswaram@zensar.com>
2021-01-27 21:50:44 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+
+namespace AgeCalculator
+{
+    [Serializable]
+    class BinarySerializationClass : IDeserializationCallback
+    {
+        public int BirthYear { get; set; }
+        
+        [NonSerialized]
+        
+        public int ageCalculated;
+        public BinarySerializationClass(int age)
+        {
+            BirthYear = age;
+        }
+        public void OnDeserialization(object obj)
+        {
+            ageCalculated = DateTime.Now.Year - BirthYear;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your birth year : ");
+            int inputYear = int.Parse(Console.ReadLine());
+            BinarySerializationClass BSC = new BinarySerializationClass(inputYear);
+            FileStream FS = new FileStream(@"AgeFile.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            BinaryFormatter BF = new BinaryFormatter();
+            BF.Serialize(FS, BSC);
+            FS.Seek(0, SeekOrigin.Begin);
+            BinarySerializationClass result = (BinarySerializationClass)BF.Deserialize(FS);
+            Console.WriteLine("\nAge of Person is " + result.ageCalculated);
+
+        }
+    }
+}
